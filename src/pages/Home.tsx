afd3c06cff79249c47adaf7de9a10d5209edc7dd
Thushara Navod <146652282+thusharanavod123@users.noreply.github.com>
2025-07-28 21:54:06 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -97,11 +97,11 @@ const Home = () => {
           </div>
         </div>
         {/* Subtle Scroll Indicator */}
-        <div className="absolute bottom-10 left-1/2 -translate-x-1/2 animate-bounce z-20">
+        {/* <div className="absolute bottom-10 left-1/2 -translate-x-1/2 animate-bounce z-20">
           <div className="w-6 h-10 border-2 border-white/30 rounded-full flex justify-center">
             <div className="w-1 h-3 bg-white/60 rounded-full mt-2 animate-pulse"></div>
           </div>
-        </div>
+        </div> */}
       </section>
 
       {/* Core Skills Section */}
